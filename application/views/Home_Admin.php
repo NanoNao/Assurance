250cd49f552bcc7ae695ea778e78bd9fba4b0186
NanoNao <benjaminsimon22@msn.com>
2017-02-16 11:55:30 +0100

    Ajout view home admin

---
+++ application/views/Home_Admin.php
@@ -0,0 +1,78 @@
+<!-- Home page des membre de l'URAPEDA-->
+<style media="screen">
+a:hover {
+  text-decoration: none;
+}
+a:visited {
+  text-decoration: none;
+}
+a:active{
+  text-decoration: none;
+}
+</style>
+<body>
+<div class="container-fluid">
+
+  <div class="row">
+    <div class="col-md-12 well">
+      <div class="col">
+        <div class="col-md-3">
+
+            <a href="#">
+            <div class="thumbnail"><!-- Affichage du bouton Sinistre -->
+            <img src="<?echo base_url();?>\assets\image\noimage.jpg" alt="Gestion des sinistres">
+            <div class="caption">
+            <h3 class = "text-center">Gestion des sinistres</h3>
+            </div>
+            </div>
+            </a>
+
+        </div>
+        <div class="col-md-3">
+
+          <a href="#">
+          <div class="thumbnail"><!-- Affichage du bouton Sinistre -->
+          <img src="<?echo base_url();?>\assets\image\noimage.jpg" alt="Gestion des sinistres">
+          <div class="caption">
+          <h3 class = "text-center">Gestion des contrats</h3>
+          </div>
+          </div>
+          </a>
+
+        </div>
+        <div class="col-md-3">
+
+          <a href="#">
+          <div class="thumbnail"><!-- Affichage du bouton Sinistre -->
+          <img src="<?echo base_url();?>\assets\image\noimage.jpg" alt="Gestion des sinistres">
+          <div class="caption">
+          <h3 class = "text-center">Gestion des Utilisateurs</h3>
+          </div>
+          </div>
+          </a>
+
+        </div>
+        <div class="col-md-3">
+          <div class="row">
+            <div class="col-md-12">
+            <h3 class = "text-center">Notifications</h3>
+            </div>
+            <div class="col-md-12">
+            </div>
+          </div>
+            <a href="#"><div class="alert alert-success" role="alert">Modification bloc [MrDupont]</div></a>
+            <a href="#"><div class="alert alert-info" role="alert">Notification 2</div></a>
+            <a href="#"><div class="alert alert-warning" role="alert">Notification 3</div></a>
+            <a href="#"><div class="alert alert-danger" role="alert">Notification 4</div></a>
+            <a href="#"><div class="alert alert-success" role="alert">Notification 5</div></a>
+
+
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+</body>
